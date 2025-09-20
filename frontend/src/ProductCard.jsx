fc72ed2fa550e590ecf0f79c1d39c66b18a4eab3
Mrcatman <unknownmister132@gmail.com>
2025-09-20 12:27:06 +0530

    password checking completed 20th Sep

--- frontend/src/ProductCard.jsx
+++ frontend/src/ProductCard.jsx
@@ -3,7 +3,7 @@
 const ProductCard = () => {
     return (
         <div className="border-2 border-blue-300 w-fit flex   items-center gap-10 p-10 rounded-2xl " >
-            <img src="./vite.svg" alt="vite photo" className="w-[100px] h-[100px] bg-gray-200 w-[100%]" />
+            <img src="./vite.svg" alt="vite photo" className="w-[100px] h-[100px] bg-gray-200 " />
             <div className="flex flex-col  gap-1">
                 <h2 className="font-bold text-xl">vite new version</h2>
                 <p className="text-lg ">most popularly used framework </p>
